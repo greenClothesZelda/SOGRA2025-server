37228fdcbc6c62c0229d7d0b03af897e3d747b56
Jinsu Kim <wlstn3697@naver.com>
2025-11-08 21:55:04 +0900

    H2

--- src/main/java/shop/byeol23/sogra2025/landmark/entity/Landmark.java
+++ src/main/java/shop/byeol23/sogra2025/landmark/entity/Landmark.java
@@ -1,5 +1,28 @@
 package shop.byeol23.sogra2025.landmark.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Landmark {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberId;
+
+	@Column(nullable = false, length = 100)
+	private String landmarkName;
+
+	// @Column(
+
 }

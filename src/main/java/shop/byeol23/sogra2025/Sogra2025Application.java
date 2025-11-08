0382ffedae356a6865ef102282d54133681c821c
Jinsu Kim <wlstn3697@naver.com>
2025-11-08 19:34:39 +0900

    FC

---
+++ src/main/java/shop/byeol23/sogra2025/Sogra2025Application.java
@@ -0,0 +1,13 @@
+package shop.byeol23.sogra2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sogra2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sogra2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/shop/byeol23/sogra2025/Sogra2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.byeol23.sogra2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sogra2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
